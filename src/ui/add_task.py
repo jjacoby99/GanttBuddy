00eef78acaf2fa27a3ba23a3649df49634454939
Josh <joshskisoccer@gmail.com>
2025-09-21 17:11:42 -0700

    improved ui for adding tasks
    now have to pick a phase

--- src/ui/add_task.py
+++ src/ui/add_task.py
@@ -1,15 +1,47 @@
 import streamlit as st
 from datetime import date, timedelta
 from models.task import Task
+from models.project import Project
+from models.phase import Phase
+import time
 
 @st.dialog("Add new task to project")
 def render_task_add(session):
-    # Inputs
+
+    phase = st.selectbox(
+        label="Select project phase to add task to.",
+        placeholder="Admin",
+        options=session.project.phases,
+        format_func=lambda t: t.name
+    )
+
+    if not phase:
+        return
+    
     task_name = st.text_input(
         label="Enter the name of a task",
         value=st.session_state.get("task_name", ""),
         key="task_name"
     )
+
+    try:
+        phase_idx = session.project.get_phase_index(phase)
+    except RuntimeError:
+        st.error(f"Project {session.project.name} doesn't contain any phases.")
+        return
+    except ValueError:
+        st.error(f"Project {session.project.name} doesn't contain a {phase.name} phase.")
+
+    task_list = session.project.phases[phase_idx].tasks
+
+    preceding_task = None
+    if task_list:
+        preceding_task = st.selectbox(
+            label="Preceding task",
+            options=task_list,
+            format_func=lambda t: "- None -" if t is None else t.name,
+            help="Select the task that directly precedes *{task_name}*"
+        )
 
     start_date, end_date = st.date_input(
         label=f"Select the start and end dates for *{task_name}*",
@@ -22,27 +54,20 @@ def render_task_add(session):
         key=f"task_note_input"
     )
 
-    task_list = session.get_tasks()
-    preceding_task = None
-    if task_list:
-        preceding_task = st.selectbox(
-            label="Preceding task",
-            options=task_list,
-            format_func=lambda t: "- None -" if t is None else t.name,
-            help="Select the task that directly precedes *{task_name}*"
+    if st.button(label=f"Add *{task_name or 'task'}* to project", disabled=not task_name):
+        new_task = Task(
+            name=task_name, 
+            start_date=start_date, 
+            end_date=end_date, 
+            note=task_note if task_note else "",
+            preceding_task=preceding_task
         )
 
-    # Submit button in the dialog
-    if st.button(label=f"Add *{task_name or 'task'}* to project", disabled=not task_name):
-        new_task = Task(name=task_name, start_date=start_date, end_date=end_date, note=task_note if task_note else "")
-        session.add_task(new_task, preceding_task)
+        session.project.add_task_to_phase(phase=phase, task=new_task,)
 
-        # Toast + clear form fields
-        st.toast(f"'{task_name}' added successfully.")
-        for el in ["task_name", "task_start_date", "task_end_date"]:
-            if el in st.session_state:
-                del st.session_state[el]
+        st.info(f"'{task_name}' added successfully.")
 
         # Close dialog and refresh main view
         st.session_state.show_add_dialog = False
+        time.sleep(1)
         st.rerun()
