ad794153cfc7fedb074dc1c664edc2e94b79fbf7
Josh <joshskisoccer@gmail.com>
2025-09-21 17:10:09 -0700

    Removed unnecessary methods from Session

--- src/models/session.py
+++ src/models/session.py
@@ -1,6 +1,6 @@
 from typing import List
-from lib.task import Task
-from lib.project import Project
+from models.task import Task
+from models.project import Project
 
 class SessionModel:
     def __init__(self):
@@ -9,17 +9,3 @@ class SessionModel:
     def set_project(self, project: Project):
         self.project = Project
 
-    def add_task(self, task: Task, preceding_task: Task = None):
-        if not preceding_task:
-            self.project.tasks.append(task) # add to end
-            return
-
-        # add after preceding_task
-        if not preceding_task in self.project.tasks:
-            raise RuntimeError(f"Preceding task {preceding_task} not found.")
-        
-        idx = self.project.tasks.index(preceding_task)
-        self.project.tasks.insert(idx + 1, task)
-
-    def get_tasks(self) -> list[Task]:
-        return self.project.tasks
