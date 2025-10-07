0817b3d8b727c586fc2d195ed43fe9959e05905e
Josh <joshskisoccer@gmail.com>
2025-10-07 15:18:46 -0700

    Cleaned up entry point

--- src/GanttBuddy.py
+++ src/GanttBuddy.py
@@ -2,17 +2,13 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import date
-
 from models.task import Task
 from models.session import SessionModel
 from models.ui_state import UIState
-from ui.add_task import render_task_add
 from ui.plot import render_gantt
 from ui.tasks_view import render_tasks_table
 from ui.settings_view import render_settings_view
 from ui.sidebar import render_project_sidebar, render_project_buttons
-from ui.add_phase import render_add_phase
-from ui.edit_phase import render_phase_edit
 
 st.set_page_config(layout="wide")
 
@@ -44,12 +40,10 @@ with settings_col:
     st.caption("Settings")
     if st.button("🔧", help="View / Edit settings: work days, hours, holidays, etc."):
             st.session_state.ui.show_settings = True
-            # st.session_state.show_settings_dialog = True
 
 if st.session_state.ui.show_settings:
     render_settings_view(st.session_state.session)
-    # st.session_state.show_settings_dialog = False
-    st.session_state.session.ui.show_settings = False
+    st.session_state.ui.show_settings = False
 
 
 if not st.session_state.session.project.phases:
