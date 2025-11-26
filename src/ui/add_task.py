105e80e4513a7db0f7ebf17fc164011b553a33d9
Josh <joshskisoccer@gmail.com>
2025-11-26 10:55:33 -0800

    bug fixes & improvements in add_task. Now takes an optional phase parameter.

--- src/ui/add_task.py
+++ src/ui/add_task.py
@@ -3,62 +3,73 @@ from datetime import date, datetime, timedelta
 from models.task import Task
 from models.project import Project
 from models.phase import Phase
+from models.session import SessionModel
 import time
 
-@st.dialog("Add new task to project")
-def render_task_add(session):
+@st.dialog("Add a new task")
+def render_task_add(session: SessionModel, phase: Phase = None):
+    if phase:
+        phase_selected = st.selectbox(
+            label="Select project phase to add task to.",
+            placeholder=phase.name,
+            options=[phase],
+            format_func=lambda t: t.name,
+            disabled=True
+        )
+    else: 
+        phase_selected = st.selectbox(
+            label="Select project phase to add task to.",
+            placeholder="Admin",
+            options=[p for p in session.project.phases.values()],
+            format_func=lambda p: p.name
+        )
 
-    phase = st.selectbox(
-        label="Select project phase to add task to.",
-        placeholder="Admin",
-        options=session.project.phases,
-        format_func=lambda t: t.name
-    )
-
-    if not phase:
+    if not phase_selected:
         return
     
     task_name = st.text_input(
         label="Enter the name of a task",
-        value=st.session_state.get("task_name", ""),
-        key="task_name"
+        key=f"{phase_selected.name}_task_name"
     )
 
-    try:
-        phase_idx = session.project.get_phase_index(phase)
-    except RuntimeError:
-        st.error(f"Project {session.project.name} doesn't contain any phases.")
-        return
-    except ValueError:
-        st.error(f"Project {session.project.name} doesn't contain a {phase.name} phase.")
-
-    task_list = session.project.phases[phase_idx].tasks
-
-    preceding_task = None
-    if task_list and st.checkbox("Set preceding task"):
-        preceding_task = st.selectbox(
-            label="Preceding task",
-            options=task_list,
-            format_func=lambda t: "- None -" if t is None else t.name,
-            help="Select the task that directly precedes *{task_name}*"
+    task_list = [t for t in phase_selected.tasks.values()]
+    
+    predecessor_ids = []
+    if task_list and st.checkbox("Has Predecessors", 
+                                 help="Select if the new task has tasks that must complete before the task begins."):
+        
+        precedessor_options = [t.uuid for t in task_list]
+        predecessor_ids = st.multiselect(
+            label="Preceding tasks",
+            options=precedessor_options,
+            format_func=lambda id: phase_selected.tasks[id].name,
+            help=f"Select all tasks that directly precede *{task_name}*"
         )
 
-    if preceding_task:
-        new_start_date = preceding_task.end_date
+    if predecessor_ids:
+        new_start_date = max(phase_selected.tasks[id].end_date for id in predecessor_ids)
+        st.info(f"Earliest start based on predecessors: **{new_start_date.strftime("%Y-%m-%d %H:%M")}**")
     
     start_col, end_col = st.columns(2)
     with start_col:
         start_day = st.date_input(
             label=f"Start date",
-            value=new_start_date if preceding_task else None,
+            value=new_start_date if predecessor_ids else None,
+            min_value=new_start_date if predecessor_ids else datetime(year=2000,month=1,day=1),
             key="task_start_date_single"
         )
+        new_start_time = session.project.settings.work_start_time
+        if predecessor_ids:
+            new_start_time = new_start_date.time() 
+
         if not session.project.settings.work_all_day:
             start_time = st.time_input(
                 label=f"Start time",
-                value=session.project.settings.work_start_time,
-                key="task_start_time"
+                value=new_start_time,
+                key=f"task_start_time_{",".join(predecessor_ids)}"
             )
+            if start_time < new_start_time:
+                st.error(f"Start time comes before earliest start: {new_start_time.strftime("%H:%M")}")
             if start_day:
                 start_date = datetime.combine(start_day, start_time)
             else:
@@ -67,7 +78,7 @@ def render_task_add(session):
     with end_col:
         end_day = st.date_input(
             label=f"End date",
-            value=new_start_date + timedelta(days=1) if preceding_task else None,
+            value=new_start_date + timedelta(days=1) if predecessor_ids else None,
             key="task_start_date"
         )
         if not session.project.settings.work_all_day:
@@ -90,16 +101,16 @@ def render_task_add(session):
         key=f"task_note_input"
     )
 
-    if st.button(label=f"Add *{task_name or 'task'}* to project", disabled=not task_name):
+    if st.button(label=f"Add", disabled=not task_name, type='primary'):
         new_task = Task(
             name=task_name, 
             start_date=start_date, 
             end_date=end_date, 
             note=task_note if task_note else "",
-            preceding_task=preceding_task
+            predecessor_ids=predecessor_ids
         )
 
-        session.project.add_task_to_phase(phase=phase, task=new_task,)
+        session.project.add_task_to_phase(phase=phase_selected, task=new_task,)
 
         st.info(f"'{task_name}' added successfully.")
 
