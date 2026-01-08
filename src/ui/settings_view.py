77c016fea7ebd68168887bf3e91ca3e24d712342
Josh <joshskisoccer@gmail.com>
2026-01-08 08:12:22 -0800

    updated settings view

--- src/ui/settings_view.py
+++ src/ui/settings_view.py
@@ -9,31 +9,72 @@ def render_settings_view(session: SessionModel):
         return
     
     settings = session.project.settings
+    work_all_day_val = settings.work_all_day
+    st.write(work_all_day_val)
+    work_all_day = st.checkbox(
+        label="Work all day?",
+        value=settings.work_all_day,
+        help="Select if work is 24 hrs/day for this project."
+    )
+    st.write(work_all_day)
+    work_start = work_end = None
 
-    with st.form("settings_form"):
+    if not work_all_day:
         col1, col2 = st.columns(2)
         with col1:
             work_start = st.time_input("Work Start Time", value=settings.work_start_time)
-            work_end = st.time_input("Work End Time", value=settings.work_end_time)
-            working_days = []
-            days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-            for i, day in enumerate(days):
-                working_days.append(st.checkbox(day, value=settings.working_days[i]))
-        
+            
         with col2:
-            province = None
-            if st.checkbox("Observe State Holidays", value=settings.observe_state_holidays):
-                province = st.selectbox(
-                    "Province (for holidays)", 
-                    options=["BC", "AB", "MB", "NB", "NL", "NS", "ON", "PE", "QC", "SK"],
-                )
+            work_end = st.time_input("Work End Time", value=settings.work_end_time)
 
-        submitted = st.form_submit_button("Save Settings")
-        if submitted:
-            settings.work_start_time = work_start
-            settings.work_end_time = work_end
-            settings.working_days = working_days
-            settings.observe_state_holidays = province is not None
-            settings.province = province
-            st.success("Settings saved.")
+    working_days = []
+    day_map = {
+            0: "Mon.",
+            1: "Tues.",
+            2: "Wed.",
+            3: "Thurs.",
+            4: "Fri.",
+            5: "Sat.",
+            6: "Sun."
+        }
+    working_days_selected = st.pills(
+        label="Working Days",
+        options=sorted(list(day_map.keys())),
+        default=[i for i, day in enumerate(session.project.settings.working_days) if day],
+        format_func=lambda d: day_map[d],
+        selection_mode="multi"
+    )
+
+    working_days_selected = sorted(working_days_selected)
+    working_days = []
+    for i in sorted(list(day_map.keys())):
+        if i in working_days_selected:
+            working_days.append(True)
+        else:
+            working_days.append(False)
+    
+    province = None
+    observe_holidays = st.checkbox(
+        "Observe Provincial Holidays", 
+        value=settings.observe_state_holidays
+    )
+
+    if observe_holidays:
+        province = st.selectbox(
+            "Province (for holidays)", 
+            options=["BC", "AB", "MB", "NB", "NL", "NS", "ON", "PE", "QC", "SK"],
+            index=0
+        )
+    
+    submitted = st.button("Save Settings")
+    if submitted:
+        settings.work_all_day = work_all_day
+        settings.work_start_time = work_start
+        settings.work_end_time = work_end
+        settings.working_days = working_days
+        settings.observe_state_holidays = province is not None
+        settings.province = province
+        st.success("Settings saved.")
+        
         st.session_state.show_settings_dialog = False
+        st.rerun()
