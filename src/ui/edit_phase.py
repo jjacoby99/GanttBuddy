9e7ed5ecc434dce521b7d0c2e285f22609005f1f
Josh <joshskisoccer@gmail.com>
2026-01-08 08:10:51 -0800

    updated edit phase. Now takes the phase to edit as a parameter.

--- src/ui/edit_phase.py
+++ src/ui/edit_phase.py
@@ -4,35 +4,41 @@ from models.phase import Phase
 from models.session import SessionModel
 
 @st.dialog(f"Edit Phase")
-def render_phase_edit(session):
-    phases = session.project.phases
+def render_phase_edit(session: SessionModel, phase: Phase):
+    proj = session.project
 
-    if not phases:
+    if not phase:
         return
     
-    phase_to_edit = st.selectbox(
-        label=f"Select a phase to edit.",
-        options=phases,
-        format_func=lambda p: p.name if p else None,
-        help="Select a phase to edit it's name or preceding task"
+    new_name = st.text_input(
+        label="Phase name",
+        value=phase.name
     )
 
-    if not phase_to_edit:
-        return
-    
-    phase_info = {
-        "Name": phase_to_edit.name,
-        "Preceding Phase": phase_to_edit.preceding_phase if phase_to_edit.preceding_phase else "-",
-    }
+    predecessor_options = [pid for pid in proj.phase_order if pid != phase.uuid]
 
-    st.divider()
-    c1, c2, c3 = st.columns(3)
-    for key, val in phase_info.items():
-        with c1:
-            st.write(key)
-        with c2:
-            st.write(val)
-        with c3:
-            st.button(f"Edit",key=f"edit_phase_{key}")
+    new_predecessors = st.multiselect(
+        label="Predecessor phases",
+        options=predecessor_options,
+        format_func=lambda pid: proj.phases[pid].name,
+        help=f"Select phases that must finish before {new_name} begins.",
+        default=phase.predecessor_ids
+    )
+    
+    with st.container(horizontal=True):
+        if st.button(label="Save"):
+            phase.name = new_name
+            phase.predecessor_ids = new_predecessors
+            st.rerun()
+        
+        st.space("stretch")
+
+        if st.button("Delete"):
+            session.project.delete_phase(phase)
+            st.rerun()
+
+    
+
+
         
 
