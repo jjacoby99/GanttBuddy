45136f18a08cb36f78707ce620311d974feb7f8b
Josh <joshskisoccer@gmail.com>
2026-01-22 07:38:31 -0800

    added helper functions to navigate execution phase index

--- src/ui/utils/phase_controls.py
+++ src/ui/utils/phase_controls.py
@@ -7,3 +7,10 @@ def prev_phase():
 def next_phase():
     phases = len(st.session_state.session.project.phase_order)
     st.session_state.phase_idx = min(phases - 1, st.session_state.ui.analysis_phase_index + 1)
+
+def prev_execution_phase():
+        st.session_state.ui.execution_phase_index = max(0, st.session_state.ui.execution_phase_index - 1)
+
+def next_execution_phase():
+    phases = len(st.session_state.session.project.phase_order)
+    st.session_state.ui.execution_phase_index = min(phases - 1, st.session_state.ui.execution_phase_index + 1)
