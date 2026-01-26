3553bd687ab6c24b3157d212f9da8c6cecc68c69
Josh <joshskisoccer@gmail.com>
2026-01-26 13:37:18 -0800

    Updates to homepage. Moved state initialization into helper function, added login, added loading from cloud

--- src/GanttBuddy.py
+++ src/GanttBuddy.py
@@ -1,14 +1,6 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from datetime import date
-
-from models.task import Task
-from models.session import SessionModel
-from models.ui_state import UIState
 
 from ui.plot import render_gantt, render_task_details
-from ui.tasks_view import render_tasks_table
 from ui.settings_view import render_settings_view
 from ui.sidebar import render_project_sidebar, render_project_buttons
 from ui.forecast_view import render_forecast
@@ -16,6 +8,15 @@ from ui.compact_buttons import use_compact_buttons
 from ui.analyze_view import render_analysis
 from ui.edit_project import render_edit_project
 from ui.render_plan import render_plan
+from ui.execution_view import render_execution_view
+
+from ui.login import render_login
+
+from logic.backend.login import is_logged_in, get_current_user, reset_auth
+from logic.backend.api_client import save_project
+
+from logic.state import initialize_session_state
+
 
 from PIL import Image
 from pathlib import Path
@@ -27,27 +28,34 @@ def load_image(path: str):
 
 st.set_page_config(layout="wide")
 
-if "session" not in st.session_state:
-    st.session_state.session = SessionModel()
+initialize_session_state()
 
-if "ui" not in st.session_state:
-    st.session_state.ui = UIState()
+if not is_logged_in():
+    render_login()
+    st.stop()
+
+
 
 use_compact_buttons()
+
 ui = st.session_state.ui
 
-# Add to ui state
-# todo
-if "show_edit_project" not in st.session_state:
-    st.session_state["show_edit_project"] = False
-
 with st.sidebar:
-    st.subheader(f"Project Explorer")
+
+    st.subheader("User")
+    user_data = get_current_user(st.session_state.get("auth_headers"))
+    with st.container(horizontal=True):
+        
+        st.caption(f":material/person: {user_data['email']}")
+        st.space("stretch")
+        if st.button(":material/logout: Logout", type="tertiary"):
+            reset_auth()
+            st.rerun()
+        
     render_project_sidebar(st.session_state.session)
 
 if st.session_state.session.project is None:
 
-    
     with st.container(horizontal_alignment="center"):
         st.info(f"Create or load a project to view.")
         path = Path(__file__).parent.resolve() / "assets" / "ganttbuddy.png"
@@ -64,29 +72,27 @@ with st.container(horizontal=True):
     st.space("stretch")
 
     if st.button(f"📝 Edit Project"):
-        st.session_state.show_edit_project = True
+        ui.show_edit_project = True
 
-if st.session_state.show_edit_project:
+if ui.show_edit_project:
     render_edit_project(st.session_state.session)
-    st.session_state.show_edit_project = False
+    ui.show_edit_project = False
 
 with st.container(horizontal=True):
     if st.button("💾", help="Save project to file"):
-        proj_dict = st.session_state.session.project.to_dict()
-        import json
-        import os
-        project_path = os.path.join(os.getcwd(),
-                                    "projects",)
-        os.makedirs(project_path, exist_ok=True)
-        with open(os.path.join(project_path, st.session_state.session.project.name + ".json"), "w") as f:
-            json.dump(proj_dict, f, default=str, indent=4)
-            st.success("Project saved.")
-
+        try:
+            save_project(st.session_state.session.project, headers=st.session_state.get("auth_headers"))
+            st.success("Project saved to server.")
+        except Exception as e:
+            st.error(f"Failed to save project to server: {e}")
+        
     st.space("stretch")
     if st.button("🔧", help="View / Edit settings: work days, hours, holidays, etc."):
             st.session_state.ui.show_settings = True
 
 
+
+# print(st.session_state.session.project.to_json())
 if st.session_state.ui.show_settings:
     render_settings_view(st.session_state.session)
     st.session_state.ui.show_settings = False
@@ -100,9 +106,20 @@ if not st.session_state.session.project.has_task:
     st.info(f"Add a task to your project to begin.")
     st.stop()
 
-task_tab, plot_tab, forecast_tab, analyze_tab = st.tabs(["Plan", "Visualize", "Forecast", "Analyze"])
+
+task_tab, execute_tab, plot_tab, forecast_tab, analyze_tab = st.tabs(
+    [":material/list_alt: Plan", 
+     ":material/play_circle: Execute", 
+     ":material/view_timeline: Visualize", 
+     ":material/trending_up: Forecast", 
+     ":material/query_stats: Analyze"]
+)
+
 with task_tab:
     render_plan(st.session_state.session)
+
+with execute_tab:
+    render_execution_view(st.session_state.session)
 
 with plot_tab:
     render_gantt(st.session_state.session)
