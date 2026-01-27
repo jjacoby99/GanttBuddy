8d35274ad1ddfe12a37a7fb261881e3cb5258239
Josh <joshskisoccer@gmail.com>
2026-01-27 13:14:54 -0800

    updated edit_task. Similar format to add_task. Now gives user the ability to set status

--- src/ui/edit_task.py
+++ src/ui/edit_task.py
@@ -4,7 +4,7 @@ from models.phase import Phase
 from models.task import Task
 from models.session import SessionModel
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 @st.dialog(f"Edit Task")
@@ -18,108 +18,146 @@ def render_task_edit(session, phase: Phase, task: Task):
         label="Edit name",
         value=task.name if task.name else ""
     )
-
-    task_dict = task.to_dict()
-    planned_df = pd.DataFrame([task_dict])
-    actual_df = pd.DataFrame([task_dict])
-
-    planned_df = planned_df.drop(["Task", "Note", "uuid", "phase_id", "predecessor_ids", "Actual_Start", "Actual_Finish"], axis=1)
-    actual_df = actual_df.drop(["Task", "Note", "uuid", "phase_id", "predecessor_ids", "Start", "Finish"], axis=1)
     
-    precdeding_task_options = ["None"] + [t for t in phase.tasks.values() if t.name != task.name]
+    task_list = [t for t in phase.tasks.values()]
+
+    predecessor_ids = task.predecessor_ids
     
-    default_predecessors = [phase.tasks[id] for id in task.predecessor_ids]
-
-    predecessors = st.multiselect(
-        label=f"Preceding Tasks",
-        options=[val for _, val in phase.tasks.items()if val.name != task.name],
-        default=default_predecessors,
-        format_func=lambda t: t.name,
-        help=f"Select tasks that must finish before {task.name} begins."
-    )
-
-    st.caption("Edit Task Plan")
-
-    edited_planned_df = st.data_editor(
-        planned_df,
-        column_config={
-            "Start": st.column_config.DatetimeColumn(
-                "Planned Start",
-                format="D MMM YYYY, h:mm a",
-                step=60,
-            ),
-            "Finish": st.column_config.DatetimeColumn(
-                "Planned End",
-                format="D MMM YYYY, h:mm a",
-                step=60,
-            ),
-        },
-        width='stretch',
-        hide_index=True
-    )
-    
-    st.caption("Edit actual start / end")
-    edited_actual_df = st.data_editor(
-        actual_df,
-        column_config={
-            "Actual_Start": st.column_config.DatetimeColumn(
-                "Actual Start",
-                format="D MMM YYYY, h:mm a",
-                step=60,
-            ),
-            "Actual_Finish": st.column_config.DatetimeColumn(
-                "Actual End",
-                format="D MMM YYYY, h:mm a",
-                step=60,
-            ),
-        },
-        hide_index=True
-    )
-
-    new_note = st.text_area(
-        label="Notes",
-        value=task.note if task.note else "",
-        help="Additional information about the task. May explain delays, context, successes, failures, etc.",
-        height="content"
-    )
-
     with st.container(horizontal=True):
-        if st.button("Save"):
-            edited_dict = edited_planned_df.iloc[0].to_dict()
+        enter_actuals = st.checkbox(
+            "Enter Actuals?",
+            value=task.completed,
+            help="Select to enter the actual start / end timestamps"
+        )
 
-            actual_edited_dict = edited_actual_df.iloc[0].to_dict()
+        st.space(size="stretch")
 
-            edited_dict = edited_dict | actual_edited_dict # merge 
-            edited_dict['Start'] = pd.to_datetime(edited_dict['Start'])
-            edited_dict['Finish'] = pd.to_datetime(edited_dict['Finish'])
-            edited_dict['Actual_Start'] = pd.to_datetime(edited_dict['Actual_Start'])
-            edited_dict['Actual_Finish'] = pd.to_datetime(edited_dict['Actual_Finish'])
-            edited_dict['Task'] = edited_task_name
-            edited_dict['predecessor_ids'] = [p.uuid for p in predecessors]
-            edited_dict['note'] = new_note
+        has_predecessors = st.checkbox(
+            "Has Predecessors", 
+            help="Select if the new task has tasks that must complete before the task begins.",
+            value=len(predecessor_ids) > 0
+        )
 
-            new_task = Task.from_dict(edited_dict)
+    if task_list and has_predecessors:
+        st.divider()
+        predecessor_ids = st.multiselect(
+            label="Preceding tasks",
+            options=[t.uuid for t in task_list],
+            default=predecessor_ids,
+            format_func=lambda id: phase.tasks[id].name,
+            help=f"Select all tasks that directly precede *{edited_task_name}*"
+        )
 
-            session.project.update_task(
-                phase=phase, 
-                old_task=task, 
-                new_task=new_task
-            )
+    new_start_date = task.start_date
+    if predecessor_ids:
+        new_start_date = max(phase.tasks[id].end_date for id in predecessor_ids)
+        st.info(f"Earliest start based on predecessors: **{new_start_date.strftime("%Y-%m-%d %H:%M")}**")
+    
+    st.divider()
+    st.caption(f"*{edited_task_name}* time stamps")
 
-            st.success("Task updated.")
-            time.sleep(1)
-            st.rerun()
+    label_col, start_col, end_col = st.columns([1,2,2])
+    with label_col:
+        st.space("small")
+        st.write("**Planned**")
 
-        st.space('stretch')
+        if enter_actuals:
+            st.space("small")
+            st.caption("")
+            st.write("**Actual**")
 
-        with st.container(horizontal=False):
-            if st.button('Delete'):
-                name = task.name
-                predecessors_had = phase.delete_task(task)
+    with start_col:
+        planned_start_dt = st.datetime_input(
+            label=f"Planned Start",
+            value=new_start_date,
+            min_value=new_start_date if predecessor_ids else datetime(year=2000,month=1,day=1),
+            key="task_start_date_single",
+            help="**Planned start** timestamp for *{edited_task_name}*"
+        )
 
-                st.info(f'\'{name}\' deleted. {predecessors_had} Tasks were preceded.')
-                time.sleep(1)
-                st.rerun()
+
+    with end_col:
+        min_end_day = task.end_date
+        if min_end_day < planned_start_dt:
+            min_end_day = planned_start_dt + timedelta(min_end_day=1)
+        
+        planned_end_dt = st.datetime_input(
+            label=f"Planned Finish",
+            value=min_end_day,
+            min_value=min_end_day if planned_start_dt else datetime.today(),
+            key="task_start_date",
+            help="**Planned end** timestamp for *{edited_task_name}*"
+        )
+
+    if enter_actuals:
+        actual_start_dt = start_col.datetime_input(
+            label="Actual Start",
+            value=None,
+            help=f"**Actual start** timestamp for *{edited_task_name}*"
+        )
+
+        actual_end_dt = end_col.datetime_input(
+            label=f"Actual Finish",
+            value=None,
+            help=f"**Actual end** timestamp for *{edited_task_name}*"
+        )
+
+
+    if not planned_start_dt or not planned_end_dt:
+        st.info("Select a start and end date to continue.")
+        st.stop()
+
+    st.divider()
+    status_map = {
+        "NOT_STARTED": ":material/schedule: Not Started",
+        "IN_PROGRESS": ":material/autorenew: In Progress",
+        "COMPLETE": ":material/check_circle: Complete",
+        "BLOCKED": ":material/block: Blocked"
+    }
+
+    task_status = st.pills(
+        label="Status",
+        options=status_map.keys(),
+        default=task.status,
+        format_func=lambda s: status_map[s],
+        help=f"Update the status of {edited_task_name}",
+        selection_mode="single"
+    )
+
+    task_note = st.text_input(
+        label=f"Add a note for '{edited_task_name}'",
+        key=f"task_note_input"
+    )
+    c1, c2, c3 = st.columns(3)
+    if c1.button(label=f"Update", disabled=not edited_task_name, type='primary'):
+        new_task = Task(
+            name=edited_task_name, 
+            start_date=planned_start_dt, 
+            end_date=planned_end_dt, 
+            actual_start=actual_start_dt if enter_actuals else None,
+            actual_end=actual_end_dt if enter_actuals else None,
+            note=task_note if task_note else "",
+            predecessor_ids=predecessor_ids,
+            status=task_status
+        )
+        session.project.update_task(phase=phase, old_task=task, new_task=new_task)
+
+        st.info(f"'{edited_task_name}' updated successfully.")
+
+        st.session_state.show_edit_dialog = False
+        time.sleep(1)
+        st.rerun()
+
+    if c2.button('Cancel'):
+        st.rerun()
+
+    if c3.button('Delete'):
+        name = task.name
+        predecessors_had = phase.delete_task(task)
+
+        st.info(f'\'{name}\' deleted. {predecessors_had} Tasks were preceded.')
+        time.sleep(1)
+        st.rerun()
 
 
         
