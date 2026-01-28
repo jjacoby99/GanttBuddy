d4304309182cbfb733ec16b6200bd03f5fa8b4b4
Josh <joshskisoccer@gmail.com>
2026-01-28 12:37:04 -0800

    updates to execution view

--- src/ui/execution_view.py
+++ src/ui/execution_view.py
@@ -31,53 +31,73 @@ def render_execution_view(session: SessionModel):
         st.info(f"task '{task.name}' has actuals enterred. Click Update Progress to overwrite.")
         
     st.header(f"Phase {phase_idx+1} of {len(session.project.phase_order)}: **{phase.name}**")
-    with st.form(f"execution_form_{task.uuid}"):
-        c1, c2, c3 = st.columns([6,2,2])
 
-        with c1:
-            st.subheader(f"Task {st.session_state.ui.execution_task_index+1} of {len(phase.task_order)}: **{task.name}**")
-
-            with st.container(horizontal=True):
-                st.metric(f"Planned Start", f"{task.start_date.strftime('%B-%d %H:%M') if task.start_date else 'N/A'}")
-                st.metric(f"Planned End", f"{task.end_date.strftime('%B-%d %H:%M') if task.end_date else 'N/A'}")
-
-
-        actual_start = c2.date_input(
-            label="Actual Start Date",
-            value=task.actual_start.date() if task.actual_start else dt.date.today(),
-            key=f"execution_actual_start_{task.uuid}",
+    with st.form(f"execution_form_{task.uuid}1", width="content"):
+        st.subheader(f"Task {st.session_state.ui.execution_task_index+1} of {len(phase.task_order)}: **{task.name}**")
+        
+        st.caption(f"Planned Start/End")
+        with st.container(horizontal=True):
+            st.metric(f"Planned Start", f"{task.start_date.strftime('%B-%d %H:%M') if task.start_date else 'N/A'}")
+            st.metric(f"Planned End", f"{task.end_date.strftime('%B-%d %H:%M') if task.end_date else 'N/A'}")
+        
+        st.caption(f"Enter Actual Start/End")
+        enter_end = st.checkbox(
+            label="Specify Task End?",
+            value=False,
+            help="Select if you want to specify the actual start and end of the task"
         )
 
-        start_time = c2.time_input(
-            label="Actual Start Time",
-            value=task.actual_start.time() if task.actual_start else dt.datetime.now().time(),
-            key=f"execution_actual_start_time_{task.uuid}",
-        )
+        with st.container():
+            c1, c2 = st.columns(2)
+            actual_start = c1.date_input(
+                label="Actual Start Date",
+                value=task.actual_start.date() if task.actual_start else dt.date.today(),
+                key=f"execution_actual_start_{task.uuid}",
+                width=100
+            )
 
-        actual_end = c3.date_input(
-            label="Actual End Date",
-            value=task.actual_end.date() if task.actual_end else dt.date.today(),
-            key=f"execution_actual_end_{task.uuid}"
-        )
+            start_time = c1.time_input(
+                label="Actual Start Time",
+                value=task.actual_start.time() if task.actual_start else dt.datetime.now().time(),
+                key=f"execution_actual_start_time_{task.uuid}",
+                width=100
+            )
 
-        end_time = c3.time_input(
-            label="Actual End Time",
-            value=task.actual_end.time() if task.actual_end else dt.datetime.now().time(),
-            key=f"execution_actual_end_time_{task.uuid}"
-        )
+            actual_end = c2.date_input(
+                label="Actual End Date",
+                value=task.actual_end.date() if task.actual_end else dt.date.today(),
+                key=f"execution_actual_end_{task.uuid}",
+                disabled=not enter_end,
+                width=100
+            )
 
-        start_dt = dt.datetime.combine(actual_start, start_time)
-        end_dt = dt.datetime.combine(actual_end, end_time)
+            end_time = c2.time_input(
+                label="Actual End Time",
+                value=task.actual_end.time() if task.actual_end else dt.datetime.now().time(),
+                key=f"execution_actual_end_time_{task.uuid}",
+                disabled=not enter_end,
+                width=100
+            )
 
-        if end_dt < start_dt:
-            st.error("End date must be after start date.")
-            return
+            start_dt = dt.datetime.combine(actual_start, start_time)
+            end_dt = dt.datetime.combine(actual_end, end_time)
+
+            if end_dt < start_dt:
+                st.error("End date must be after start date.")
+                return
+            
+            note = st.text_area(
+                label="Execution Note",
+                placeholder="Enter relevant details about the task here. Think delays, rationale, wins, lossess, etc.",
+                height=400,
+                value=task.note if task.note else None
+            )
 
         submitted = st.form_submit_button(":material/event_available: Update Progress", type="primary")
         if submitted:
             task.actual_start = start_dt
             task.actual_end = end_dt
-
+            task.note = note
             st.success(f"Updated actuals for task **{task.name}**.")
 
             # incrememnt the task index
