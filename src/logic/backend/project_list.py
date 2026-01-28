cd68a31e6276a880e802de81d77642f2c62e1f99
Josh <joshskisoccer@gmail.com>
2026-01-28 12:10:52 -0800

    updated get_projects function to include more data about projects from the backend to display

--- src/logic/backend/project_list.py
+++ src/logic/backend/project_list.py
@@ -1,6 +1,7 @@
 from logic.backend.api_client import fetch_projects
+import datetime as dt
 
-def get_projects(headers) -> dict[str, str]:
+def get_projects(headers) -> dict:
     """
         Returns a dict of project_id: project_name
     """
@@ -14,7 +15,20 @@ def get_projects(headers) -> dict[str, str]:
     for proj in resp:
         id = proj.get("id", None)
         name = proj.get("name", None)
+        description = proj.get("description", None)
+        created = proj.get("created_at", None)
+        if created:
+            dt_created = dt.datetime.fromisoformat(created.replace("Z", "+00:00")).astimezone(dt.timezone.utc)
     
-        to_return[id] = name
+        updated = proj.get("updated_at", None)
+        if updated:
+            dt_updated = dt.datetime.fromisoformat(updated.replace("Z", "+00:00")).astimezone(dt.timezone.utc)
+
+        to_return[id] = {
+            "name": name,
+            "description": description,
+            "created": dt_created,
+            "updated": dt_updated
+        }
     
     return to_return
