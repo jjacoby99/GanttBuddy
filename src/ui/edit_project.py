ba27e66563b1cfbe424ea1f87b0724d36df2f3ab
Josh <joshskisoccer@gmail.com>
2026-02-05 12:32:16 -0800

    Added ability to edit project type in edit_project

--- src/ui/edit_project.py
+++ src/ui/edit_project.py
@@ -5,15 +5,27 @@ from models.project import Project
 from models.phase import Phase
 from models.task import Task
 
+from models.project_type import ProjectType
+
 @st.dialog("Edit Project")
 def render_edit_project(session: SessionModel):
 
     project = session.project
 
-    new_name = st.text_input("Project Name", value=project.name)
+    new_name = st.text_input("Name", value=project.name)
+
+    description = st.text_area("Description", value=project.description)
+
+    type = st.pills(
+        label="Specify Project Type",
+        options=list(ProjectType),
+        format_func=lambda s: s.name.replace("_"," ").capitalize()
+    )
 
     if st.button("Save Changes"):
         project.name = new_name
+        project.description = description
+        project.project_type = type
         st.success("Project updated.")
         st.session_state.ui.show_rename_project = False
         st.rerun()
