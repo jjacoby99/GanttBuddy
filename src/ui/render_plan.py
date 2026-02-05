4bb4cdb11d430d719d266db9aaeac9c6e976e741
Josh <joshskisoccer@gmail.com>
2026-02-05 11:24:39 -0800

    moved inline function definition from render_plan

--- src/ui/render_plan.py
+++ src/ui/render_plan.py
@@ -6,8 +6,7 @@ from models.shift_schedule import ShiftSchedule
 from ui.phases_view import render_phases_view
 from ui.tasks_view import render_tasks_table
 
-from ui.shift_config import render_shift_schedule_table, render_tz_info
-
+from ui.edit_schedule import edit_shift_schedule
 from zoneinfo import ZoneInfo
 
 # @st.cache_data: throws Unhashable Error for SessionModel
@@ -23,37 +22,7 @@ def render_plan(session: SessionModel):
             format_func=lambda t: options[t],
             width=300
         )
-        st.space("stretch")
-
-        with st.container():
-            st.space("small")
-            edit_schedule = st.button(
-                label="Shift Schedule",
-                icon=":material/calendar_month:",
-                help="Click to edit the shift schedule associated with the project"
-            )
-
-        if edit_schedule:
-            @st.dialog(f"Edit Shift Schedule")
-            def edit_shift_schedule():
-                tz = render_tz_info(edit=True)
-                df = render_shift_schedule_table(edit=True)
-                c1, c2 = st.columns(2)
-                if c1.button("Update Schedule"):            
-                    try:
-                        sched = ShiftSchedule.from_df(df)
-                        sched.timezone = ZoneInfo(tz)
-                    except Exception as e:
-                        st.error(f"Error updating project schedule: {e}")
-                        return
-                    session.project.shift_schedule = sched
-                    st.success(f"Schedule updated successfully")
-                    st.rerun()
-                
-                if c2.button("Cancel"):
-                    st.rerun()
-            
-            edit_shift_schedule()
+        
 
     match view_option:
         case "Simple":
