02a5513babd04235d44a3c2b1705eb68e78a8c31
Josh <joshskisoccer@gmail.com>
2026-02-12 07:29:24 -0800

    added ui for shift_assignment.

--- src/ui/shift_config.py
+++ src/ui/shift_config.py
@@ -3,7 +3,9 @@ import streamlit as st
 from models.shift_schedule import Shift, ShiftSchedule
 from models.session import SessionModel
 from models.project import Project
+from models.crew import CrewIn
 
+from ui.add_crew import add_crew
 import datetime as dt
 
 import pandas as pd
@@ -41,68 +43,73 @@ def render_tz_info(current_tz = None):
 
     return tz[0]
 
-def render_shift_schedule_table(edit: bool = False):
+from models.shift_schedule import ShiftAssignment, assignments_to_df
+
+def render_shift_assignment_table(crews: list[CrewIn], project_id: str, edit: bool = False) -> pd.DataFrame:
     st.subheader(f"Shift Schedule")
-    if not edit or st.session_state.session.project.shift_schedule is None:
-        default_day = Shift(
-            start=dt.time(hour=7),
-            duration=dt.timedelta(hours=12),
+    
+    shift_assignments = st.session_state.get("shift_assignments", None)
+
+    if not crews:
+        st.info("No crews available for the selected site. Make some!")
+        if st.button(f"Add Crew"):
+            add_crew()
+        return None
+    
+    # initialize some sensible defaults
+    if not shift_assignments:
+        a1 = ShiftAssignment(
+            project_id=project_id,
+            crew_id=next(iter([c.id for c in crews])),
             shift_type="day",
-            crew="A Crew"
+            start_date=dt.date.today(),
+            end_date=dt.date.today() + dt.timedelta(days=4)
         )
-
-        default_night = Shift(
-            start=dt.time(hour=19),
-            duration=dt.timedelta(hours=12),
+        a2 = ShiftAssignment(
+            project_id=project_id,
+            crew_id=next(iter([c.id for c in crews])),
             shift_type="night",
-            crew="B Crew"
+            start_date=dt.date.today(),
+            end_date=dt.date.today() + dt.timedelta(days=4)
         )
+        shift_assignments = [a1, a2]
 
-        schedule = ShiftSchedule(
-            shifts=[default_day, default_night]
-        )
-    elif st.session_state.session.project.shift_schedule is not None:
-        schedule = st.session_state.session.project.shift_schedule
-   
+    crew_id_map = {crew.id: crew.name for crew in crews}
+    
     st.caption("Set your schedule.")
-    data = schedule.to_dict()
-    df = pd.DataFrame(data)
-
-    view = df.drop("duration", axis=1)
-    with st.container():
-        edited = st.data_editor(
-            data=view,
-            column_order=["shift_type", "start", "end", "crew"],
-            width="stretch",
-            num_rows='dynamic',
-            column_config={
-                "crew": st.column_config.TextColumn(
-                    label="Crew(s)",
-                    required=False,
-                    default=""
-                ),
-                "start": st.column_config.TimeColumn(
-                    label="Start Time",
-                    required=True,
-                    default=dt.time(hour=7),
-                    format="hh:mm a",
-                    step=dt.timedelta(minutes=15)
-                ),
-                "end": st.column_config.TimeColumn(
-                    label="End Time",
-                    required=True,
-                    default=dt.time(hour=19),
-                    format="hh:mm a",
-                    step=dt.timedelta(minutes=15)
-                ),
-                "shift_type": st.column_config.SelectboxColumn(
-                    label="Shift Type",
-                    required=True,
-                    options=["day", "night"],
-                    format_func=lambda t: t.capitalize()
-                )
-            }
-        )
-        return edited
+    
+    df = assignments_to_df(shift_assignments)
+    
+    edited = st.data_editor(
+        data=df,
+        column_order=["crew_id", "shift_type", "start_date", "end_date"],
+        width="stretch",
+        num_rows='dynamic',
+        column_config={
+            "crew_id": st.column_config.SelectboxColumn(
+                label="Crew",
+                required=True,
+                options=list(crew_id_map.keys()),
+                format_func=lambda c: crew_id_map[c]
+            ),
+            "start_date": st.column_config.DateColumn(
+                label="Start Date",
+                required=True,
+                default=dt.date.today(),
+            ),
+            "end_date": st.column_config.DateColumn(
+                label="End Date",
+                required=True,
+                default=dt.date.today(),
+            ),
+            "shift_type": st.column_config.SelectboxColumn(
+                label="Shift Type",
+                required=True,
+                options=["day", "night"],
+                format_func=lambda t: t.capitalize()
+            )
+        }
+    )
+    return edited
     
 
