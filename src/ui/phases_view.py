f96b4c895b880bf47c92af1acaa95a07bd9656d1
Josh <joshskisoccer@gmail.com>
2026-02-21 11:12:38 -0800

    Updated phases_view dataframe

--- src/ui/phases_view.py
+++ src/ui/phases_view.py
@@ -21,6 +21,28 @@ def render_phases_view(session: SessionModel):
     st.subheader("Project Phases Overview")
     
     phase_df = session.project.get_phase_df()
-    phase_df = phase_df[["phase", "planned_start", "planned_end", "planned_duration", "num_tasks"]]
-    phase_df.columns = ["Phase", "Planned Start", "Planned End", "Planned Duration (hrs)", "Number of Tasks"]
-    st.data_editor(phase_df)
+    view = phase_df[["phase", "planned_start", "planned_end", "planned_duration", "num_tasks"]]
+    st.dataframe(
+        data=view,
+        column_order=["phase", "planned_start", "planned_end", "planned_duration", "num_tasks"],
+        column_config={
+            "phase": st.column_config.TextColumn("Phase Name"),
+            "planned_start": st.column_config.DatetimeColumn(
+                label="Planned Start",
+                format="localized",
+                ),
+            "planned_end": st.column_config.DatetimeColumn(
+                label="Planned End",
+                format="localized",
+                ),
+            "planned_duration": st.column_config.NumberColumn(
+                label="Planned Duration (hrs)",
+                width="small",
+                format="%.1f"
+                ),
+            "num_tasks": st.column_config.NumberColumn(
+                label="Number of Tasks",
+                width="small"
+                ),
+        }
+    )
