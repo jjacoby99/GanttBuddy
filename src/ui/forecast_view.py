e0b0e759a6c8594e3d5d7df28fd708f7162e580a
Josh <joshskisoccer@gmail.com>
2026-02-21 18:10:57 -0800

    updates to forecast for mill relines

--- src/ui/forecast_view.py
+++ src/ui/forecast_view.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import streamlit as st
 import pandas as pd
 from models.session import SessionModel
@@ -30,21 +31,63 @@ def render_forecast(session: SessionModel):
         st.metric("Planned End", res.planned_end.strftime(DTFMT) if not pd.isna(res.planned_end) else "—")
         
         st.space(f"stretch")
-
+        proj = session.project
+        variance = 0.0
+        for pid in proj.phase_order:
+            phase = proj.phases[pid]
+            for tid in phase.task_order:
+                task = phase.tasks[tid]
+                if task.completed:
+                    variance += task.variance.total_seconds() / 3600
+        last_task_completed = None
+        last_task = None
+        for pid in proj.phase_order:
+            phase = proj.phases[pid]
+            for tid in phase.task_order:
+                task = phase.tasks[tid]
+                if not task.completed:
+                    last_task_completed = last_task
+                    break
+                last_task = task
+        if last_task_completed is not None:
+            delay = last_task_completed.actual_end - last_task_completed.end_date
+        else: delay = proj.actual_end - proj.end_date
+        # st.metric(
+        #     "Forecast End",
+        #     res.forecast_end.strftime(DTFMT) if not pd.isna(res.forecast_end) else "—",
+        #     delta=(
+        #         ("+" if (not pd.isna(res.planned_end) and not pd.isna(res.forecast_end) and res.forecast_end > res.planned_end) else "")
+        #         + (str(format_timedelta(res.forecast_end - res.planned_end)) if (not pd.isna(res.planned_end) and not pd.isna(res.forecast_end)) else "")
+        #     ),
+        #     delta_color="inverse"
+        # )
+        forecast_end = proj.end_date + delay #- timedelta(hours=1.5)
         st.metric(
             "Forecast End",
-            res.forecast_end.strftime(DTFMT) if not pd.isna(res.forecast_end) else "—",
+            forecast_end.strftime(DTFMT),
             delta=(
                 ("+" if (not pd.isna(res.planned_end) and not pd.isna(res.forecast_end) and res.forecast_end > res.planned_end) else "")
-                + (str(format_timedelta(res.forecast_end - res.planned_end)) if (not pd.isna(res.planned_end) and not pd.isna(res.forecast_end)) else "")
+                + (str(format_timedelta(delay)) if delay is not None else "")
             ),
             delta_color="inverse"
         )
-
         st.space(f"stretch")
 
-        st.metric("Completed (to date)", f"{session.project.completed_hours():.1f} h")
+        actual, planned = session.project.completed_hours()
+        # actual += timedelta(hours=1.5).total_seconds() / 3600
+        # planned += timedelta(hours=1.5).total_seconds() / 3600
+        st.metric(
+            label="Earned (to date)", 
+            value=f"{actual:.1f} h",
+            delta=f"{format_timedelta(timedelta(hours=actual - planned))}",
+            delta_color="inverse",
+            help="Total *actual* hours completed across all **planned** tasks vs the *planned* hours for those tasks."
+        )
         
+        st.space(f"stretch")
+
+        st.metric("Unplanned (to date)", f"{session.project.unplanned_hours():.1f} h")
+
         st.space(f"stretch")
         
         st.metric("Total Planned", f"{res.planned_curve.iloc[-1]:,.1f} h")
@@ -54,7 +97,7 @@ def render_forecast(session: SessionModel):
     st.plotly_chart(fig, width='stretch')
     
     if res.forecast_start is not None:
-        st.info(f"Forecast start: {res.forecast_start.strftime("%b %d %H:%M")}", width=220)
+        st.info(f"Last Update: {res.forecast_start.strftime("%b %d %H:%M")}", width=220)
 
     # Details
     show_table = st.checkbox("Show forecasted task details", value=False)
