a9b78200edcc7372c6befaa9aafeb38854e21db7
Josh <joshskisoccer@gmail.com>
2026-02-26 06:58:34 -0800

    added plan_state to initialize_session_state

--- src/logic/state.py
+++ src/logic/state.py
@@ -2,7 +2,7 @@ import streamlit as st
 from models.session import SessionModel
 from models.ui_state import UIState
 from models.gantt_state import GanttState
-
+from models.plan_state import PlanState
 def initialize_session_state():
     if "access_token" not in st.session_state:
         st.session_state["access_token"] = None
@@ -16,6 +16,8 @@ def initialize_session_state():
         st.session_state.ui = UIState()
     if "gantt_state" not in st.session_state:
         st.session_state.gantt_state = GanttState()
+    if "plan_state" not in st.session_state:
+        st.session_state.plan_state = PlanState()
 
 
 def state_initialized() -> bool:
