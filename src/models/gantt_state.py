b2540d6f8263b69e91196e09c5cef62523c19920
Josh <joshskisoccer@gmail.com>
2026-03-02 15:44:26 -0800

    updatead gantt state class to store delay windows.

--- src/models/gantt_state.py
+++ src/models/gantt_state.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import datetime as dt
 
 @dataclass
@@ -9,6 +9,8 @@ class GanttState:
     bta_colors: bool = True
     shade_non_working_time: bool = True
 
+    show_delay_windows: bool = False
+    selected_delay_types: list[str] = field(default_factory=list)
     x_axis_start: dt.datetime = None
     x_axis_end: dt.datetime = None
 
