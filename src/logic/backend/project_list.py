f78c4be2979bb38810f9f5d61ce1ab4aae3abb76
Josh <joshskisoccer@gmail.com>
2026-03-13 14:52:23 -0700

    updated get_projects to apply local timezone

--- src/logic/backend/project_list.py
+++ src/logic/backend/project_list.py
@@ -1,5 +1,6 @@
 from logic.backend.api_client import fetch_projects
 import datetime as dt
+from zoneinfo import ZoneInfo
 
 def get_projects(headers: dict, n_proj: int=-1) -> dict:
     """
@@ -21,21 +22,28 @@ def get_projects(headers: dict, n_proj: int=-1) -> dict:
         name = proj.get("name", None)
         description = proj.get("description", None)
 
+        tz_str = proj.get("timezone_name", None)
+        if not tz_str:
+            raise ValueError(f"Project response must contain a local timezone.")
+
+        tz = ZoneInfo(tz_str)
+
         created = proj.get("created_at", None)
         if created:
-            dt_created = dt.datetime.fromisoformat(created.replace("Z", "+00:00")).astimezone(dt.timezone.utc)
+            dt_created = dt.datetime.fromisoformat(created.replace("Z", "+00:00")).astimezone(tz=tz)
     
         updated = proj.get("updated_at", None)
         if updated:
-            dt_updated = dt.datetime.fromisoformat(updated.replace("Z", "+00:00")).astimezone(dt.timezone.utc)
+            dt_updated = dt.datetime.fromisoformat(updated.replace("Z", "+00:00")).astimezone(tz=tz)
 
         start = proj.get("planned_start", None)
         if start:
-            start_dt = dt.datetime.fromisoformat(updated.replace("Z", "+00:00")).astimezone(dt.timezone.utc)
+            start_dt = dt.datetime.fromisoformat(start.replace("Z", "+00:00")).astimezone(tz=tz)
 
         end = proj.get("planned_finish", None)
         if start:
-            end_dt = dt.datetime.fromisoformat(updated.replace("Z", "+00:00")).astimezone(dt.timezone.utc)
+            end_dt = dt.datetime.fromisoformat(end.replace("Z", "+00:00")).astimezone(tz=tz)
+
 
         to_return[id] = {
             "name": name,
@@ -43,7 +51,7 @@ def get_projects(headers: dict, n_proj: int=-1) -> dict:
             "created": dt_created,
             "updated": dt_updated,
             "planned_start": start_dt,
-            "planned_finish": end_dt
+            "planned_finish": end_dt,
         }
     
     return to_return
