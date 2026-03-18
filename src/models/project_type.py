37a2fd2e6ed8e2e16d4b4e78d25f8a4befdf27b9
Josh <joshskisoccer@gmail.com>
2026-03-18 15:45:39 -0700

    added project_type_to_dict helper

--- src/models/project_type.py
+++ src/models/project_type.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from enum import Enum
 
 class ProjectType(Enum):
@@ -5,3 +7,6 @@ class ProjectType(Enum):
     CRUSHER_REBUILD = 2
     CIVIL=3
     GENERIC=4
+
+def project_type_to_dict() -> dict[str, ProjectType]:
+    return {" ".join([part.capitalize() for part in pt.name.split("_")]): pt for pt in list(ProjectType)}
