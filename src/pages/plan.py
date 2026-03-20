8f61ea6ce9a57a72bbca3af9efa90d11e28c3deb
Josh <joshskisoccer@gmail.com>
2026-03-20 09:17:57 -0700

    added closeout button to plan

--- src/pages/plan.py
+++ src/pages/plan.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from ui.plot import render_gantt, render_task_details
 from ui.render_plan import render_plan
+from ui.closeout import render_closeout
 
 from ui.utils.workspace import render_workspace_buttons
 
@@ -21,3 +22,14 @@ with plot_tab:
     render_gantt(st.session_state.session)
     st.divider()
     render_task_details(st.session_state.session)
+
+
+if not st.session_state.session.project.closed:
+    close_button = st.button(
+        label=":material/task_alt: Closeout Project",
+        help="Initiate project closeout process. Closed projects no longer show up on feed & home pages.",
+        type="primary"
+    ) 
+
+    if close_button:
+        render_closeout(session=st.session_state.session)
