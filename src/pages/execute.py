90a09b8e0c05377013ddef4925debbba0813fc78
Josh <joshskisoccer@gmail.com>
2026-04-23 07:43:46 -0700

    Updated execute page to consume new page header helper

--- src/pages/execute.py
+++ src/pages/execute.py
@@ -2,8 +2,11 @@ import streamlit as st
 
 from ui.forecast_view import render_forecast
 from ui.execution_view import render_execution_view
+from ui.utils.page_header import render_registered_page_header
 from ui.utils.workspace import render_workspace_buttons
 
+project = st.session_state.session.project
+render_registered_page_header("execute", chips=[project.name] if project is not None else [])
 
 render_workspace_buttons()
 
