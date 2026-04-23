78ed35b670e11c25fa5dc526c4d9ae48fbb816fa
Josh <joshskisoccer@gmail.com>
2026-04-23 07:46:35 -0700

    Updated todo page to consume new page header helper

--- src/pages/todo.py
+++ src/pages/todo.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 from ui.todo_list import render_todo_list
+from ui.utils.page_header import render_registered_page_header
 
 
-st.header("Todos")
+render_registered_page_header("todos")
 render_todo_list()
