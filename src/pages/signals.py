a7cb8bcedba7b4147db41b86bfac1c7e22fdc468
Josh <joshskisoccer@gmail.com>
2026-04-23 07:46:24 -0700

    Updated signals page to consume new page header helper

--- src/pages/signals.py
+++ src/pages/signals.py
@@ -2,13 +2,17 @@ import streamlit as st
 
 from logic.feature_flags import signals_enabled
 from ui.signals_view import render_signals_view
+from ui.utils.page_header import render_registered_page_header
 from ui.utils.workspace import render_workspace_buttons
 
 if not signals_enabled():
-    st.title("Signals")
+    render_registered_page_header("signals", chips=["Feature preview"])
     st.info(":material/info: Coming soon!")
     st.caption("We're working hard to bring you actionable insights and alerts to keep your projects on track. Stay tuned!")
     st.stop()
+
+project = st.session_state.session.project
+render_registered_page_header("signals", chips=[project.name] if project is not None else [])
 
 render_workspace_buttons()
 render_signals_view(st.session_state.session)
