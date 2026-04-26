e10b774ea88e3b7cc0404df5fa83c46f66a570ac
Josh <joshskisoccer@gmail.com>
2026-04-26 16:56:40 -0700

    Updated get_projects

--- src/logic/backend/project_list.py
+++ src/logic/backend/project_list.py
@@ -1,10 +1,10 @@
 from logic.backend.api_client import fetch_projects
-import datetime as dt
-from zoneinfo import ZoneInfo
 
-def get_projects(headers: dict, n_proj: int=-1, include_closed: bool = False) -> dict:
+from models.project_summary import ProjectSummary
+
+def get_projects(headers: dict, n_proj: int=-1, include_closed: bool = False) -> dict[str, ProjectSummary]:
     """
-        Returns a dict of project_id: project_name
+        Returns a dict of project_id: project summary.
     """
     if n_proj < -1:
         raise ValueError(f"n_proj must be positive (or -1 for all projects)")
@@ -16,48 +16,9 @@ def get_projects(headers: dict, n_proj: int=-1, include_closed: bool = False) ->
     if n_proj != -1:
         resp = resp[:n_proj]
 
-    to_return = {}
+    to_return: dict[str, ProjectSummary] = {}
     for proj in resp:
-        id = proj.get("id", None)
-        name = proj.get("name", None)
-        description = proj.get("description", None)
+        summary = ProjectSummary.model_validate(proj)
+        to_return[summary.id] = summary
 
-        tz_str = proj.get("timezone_name", None)
-        if not tz_str:
-            raise ValueError(f"Project response must contain a local timezone.")
-
-        tz = ZoneInfo(tz_str)
-
-        created = proj.get("created_at", None)
-        if created:
-            dt_created = dt.datetime.fromisoformat(created.replace("Z", "+00:00")).astimezone(tz=tz)
-    
-        updated = proj.get("updated_at", None)
-        if updated:
-            dt_updated = dt.datetime.fromisoformat(updated.replace("Z", "+00:00")).astimezone(tz=tz)
-
-        start = proj.get("planned_start", None)
-        if start:
-            start_dt = dt.datetime.fromisoformat(start.replace("Z", "+00:00")).astimezone(tz=tz)
-
-        end = proj.get("planned_finish", None)
-        if start:
-            end_dt = dt.datetime.fromisoformat(end.replace("Z", "+00:00")).astimezone(tz=tz)
-
-
-        to_return[id] = {
-            "name": name,
-            "description": description,
-            "created": dt_created,
-            "updated": dt_updated,
-            "planned_start": start_dt if start else None,
-            "planned_finish": end_dt if start else None,
-            "timezone_name": tz_str,
-            "created_by_user_id": proj.get("created_by_user_id"),
-            "can_view": proj.get("can_view"),
-            "can_edit": proj.get("can_edit"),
-            "can_manage_members": proj.get("can_manage_members"),
-            "can_delete": proj.get("can_delete"),
-        }
-    
     return to_return
